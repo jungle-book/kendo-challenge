6ffb7d6d0a3047185fb38ad3d28884cec93e067f
rohit20001221 <rohit20001221@gmail.com>
2025-03-22 12:24:39 +0530

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import react from '@vitejs/plugin-react-swc'
-import { defineConfig } from 'vite'
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-    TanStackRouterVite({target: "react", autoCodeSplitting: true}),
-    react()
+    TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
+    react(),
   ],
-})
+  preview: {
+    port: 8080,
+    strictPort: true,
+    host: "0.0.0.0",
+  },
+});
