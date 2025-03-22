b90d6b0db396222664f521d6d4d09da6ef2fc889
rohit20001221 <rohit20001221@gmail.com>
2025-03-23 01:38:04 +0530

    sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -4,6 +4,13 @@ import { SidebarProfile } from "../SidebarProfile";
 import styles from "./sidebar.module.css";
 import { SidebarLink } from "../SidebarLink";
 
+import mixesIcon from "@/assets/mixes.svg";
+import playlistIcon from "@/assets/playlist.svg";
+import albumsIcon from "@/assets/albums.svg";
+import tracksIcon from "@/assets/tracks.svg";
+import videosIcon from "@/assets/videos.svg";
+import artistsIcon from "@/assets/artists.svg";
+
 export const Sidebar: FC = () => {
   return (
     <div className={styles["sidebar__container"]}>
@@ -15,15 +22,27 @@ export const Sidebar: FC = () => {
         <SidebarLink label="Videos" />
       </div>
 
-      <div>
-        <span>MY COLLECTION</span>
+      <div className={styles["sidebar__section"]}>
+        <span className={styles["sidebar__section__title"]}>MY COLLECTION</span>
+        <div>
+          <SidebarLink label="Mixes and Radio" startIcon={mixesIcon} />
+          <SidebarLink label="Playlist" startIcon={playlistIcon} />
+          <SidebarLink label="Albums" startIcon={albumsIcon} />
+          <SidebarLink label="Tracks" startIcon={tracksIcon} />
+          <SidebarLink label="Videos" startIcon={videosIcon} />
+          <SidebarLink label="Artists" startIcon={artistsIcon} />
+        </div>
+      </div>
+
+      <div className={styles["sidebar__section"]}>
+        <span className={styles["sidebar__section__title"]}>MY PLAYLIST</span>
         <div>
           <SidebarLink label="Mixes and Radio" />
-          <SidebarLink label="Playlist" />
-          <SidebarLink label="Albums" />
+          <SidebarLink label="September" />
+          <SidebarLink label="Clubbing" />
+          <SidebarLink label="Chill story2" />
+          <SidebarLink label="Playlist 342" />
           <SidebarLink label="Tracks" />
-          <SidebarLink label="Videos" />
-          <SidebarLink label="Artists" />
         </div>
       </div>
     </div>
--- src/components/SidebarLink/SidebarLink.tsx
+++ src/components/SidebarLink/SidebarLink.tsx
@@ -1,15 +1,16 @@
 import { FC } from "react";
 
 import styles from "./link.module.css";
-
-export const SidebarLink: FC<SidebarLinkProps> = ({ label }) => {
+export const SidebarLink: FC<SidebarLinkProps> = ({ label, startIcon }) => {
   return (
     <div className={styles["container"]}>
-      <p>{label}</p>
+      {startIcon && <img src={startIcon} width={24} height={24} />}
+      <p className={styles["label"]}>{label}</p>
     </div>
   );
 };
 
 type SidebarLinkProps = {
   label: string;
+  startIcon?: string;
 };
