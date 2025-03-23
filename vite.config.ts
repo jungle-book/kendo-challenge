df7977727a27f265817c67e1d60d63243970e4c2
rohit20001221 <rohit20001221@gmail.com>
2025-03-23 14:24:10 +0530

    .

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,14 @@
 import path from "path";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import react from "@vitejs/plugin-react-swc";
+import svgr from "@svgr/rollup";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     TanStackRouterVite({ target: "react", autoCodeSplitting: true }),
+    svgr({ exportType: "default" }),
     react(),
   ],
   resolve: {
